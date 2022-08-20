914598c0a6eef897856334638243744ba35796b5
AFreul kaium Joy <afreuljoy@gmail.com>
2022-08-20 21:20:16 +0600

    pagination done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Images from "./Components/Images";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/albums/1/photos`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data);
+      });
+  }, []);
+
+  return(
+    <>
+      <Images data={images} />
+    </>
+  ) ;
 }
 
 export default App;
---
+++ src/Components/Images.js
@@ -0,0 +1,69 @@
+import { useEffect,useState} from "react";
+import ReactPaginate from "react-paginate";
+import '../App.css'
+
+export default function Images (props) {
+    const {data} = props; 
+    const [currentItems, setCurrentItems] = useState([]); // show in page 
+    const [pageCount, setPageCount] = useState(0); // 
+    const [itemOffset, setItemOffset] = useState(0); //index of the first item of currentPage
+    const itemsPerPage = 6;
+    
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage; //index of the last item of currentPage
+        console.log(`Loading data from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(data.slice(itemOffset, endOffset)); // update currentItems
+        setPageCount(Math.ceil(data.length / itemsPerPage)); // update the page count
+    }, [itemOffset, itemsPerPage,data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
+  return (
+    <>
+      <div className="homePage">
+      <> 
+              {currentItems.map(image =>{
+                  return ( 
+                      <div> 
+                          <img src={image.url} alt="" />
+                      </div>
+                  );
+              })}
+          </>
+      </div>
+
+      <div className="pageResponsive">
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName="pagination"
+        pageLinkClassName="page-num"
+        previousLinkClassName="previousNext"
+        nextLinkClassName="previousNext"
+        activeLinkClassName="active"
+      />
+      </div>
+
+    </>
+  );
+}
+
+
+
+
+
+
+
+
+
